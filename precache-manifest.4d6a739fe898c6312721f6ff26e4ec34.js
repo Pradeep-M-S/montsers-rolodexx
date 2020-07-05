13d1fc79ed0930a546fe86a1c4653e4205094d77
Pradeep-M-S <pradeepms200076@gmail.com>
2020-07-06 00:18:35 +0530

    Updates

---
+++ precache-manifest.4d6a739fe898c6312721f6ff26e4ec34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc4e4a08bfdbac62cb27c98103d2793",
+    "url": "/montsers-rolodexx/index.html"
+  },
+  {
+    "revision": "3a51e45826fcf115544d",
+    "url": "/montsers-rolodexx/static/css/main.98b457d2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/montsers-rolodexx/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/montsers-rolodexx/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a51e45826fcf115544d",
+    "url": "/montsers-rolodexx/static/js/main.68ff57f9.chunk.js"
+  },
+  {
+    "revision": "374052c8775de3022fd2",
+    "url": "/montsers-rolodexx/static/js/runtime-main.9460e75d.js"
+  }
+]);
